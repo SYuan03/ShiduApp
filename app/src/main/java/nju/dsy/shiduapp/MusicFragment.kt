c0a86774f2c53ef5e80a0118a3b10f1980494928
ShengyuanD <211250097@smail.nju.edu.cn>
2023-07-31 17:09:24 +0800

    finish music endless play

--- app/src/main/java/nju/dsy/shiduapp/MusicFragment.kt
+++ app/src/main/java/nju/dsy/shiduapp/MusicFragment.kt
@@ -8,9 +8,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import nju.dsy.shiduapp.music.Music
 import nju.dsy.shiduapp.music.MusicAdapter
+
 
 //class MusicFragment : Fragment() {
 //    val TAG = "MusicFragment"
@@ -45,22 +47,29 @@ class MusicFragment : Fragment() {
 
     private lateinit var musicRecyclerView: RecyclerView
     private lateinit var musicAdapter: MusicAdapter
+    private var currentPlayingPosition = -1
 
-    private val musicList = listOf(
-        Music("daxiaojianghu", R.raw.music_daxiaojianghu),
-        Music("stories", R.raw.music_stories),
-        Music("the_ludlows", R.raw.music_the_ludlows),
-        Music("geiniyiping", R.raw.music_geiniyiping),
+    private val musicList: MutableList<Music> = mutableListOf()
+
+    private val originalMusicList = listOf(
+        Music("大笑江湖 - 小沈阳", R.raw.music_daxiaojianghu),
+        Music("Bones - Imagine Dragons", R.raw.music6),
+        Music("Cure For Me - Aurora", R.raw.music5),
+        Music("给你一瓶魔法药水 - 告五人", R.raw.music_geiniyiping),
+        Music("If We Ever Broke Up - Mae Stephens", R.raw.music4),
+        Music("Never Gonna Give You Up - Rick Astley", R.raw.music3),
     )
 
     private var mediaPlayer: MediaPlayer? = null
     private var isPlaying = false
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_music, container, false)
+
         musicRecyclerView = view.findViewById(R.id.musicRecyclerView)
 
         // 初始化RecyclerView和Adapter
@@ -70,29 +79,72 @@ class MusicFragment : Fragment() {
             if (isPlaying) {
                 stopMusic()
             } else {
-                playMusic(music.resourceId)
+                playMusic(music.resourceId, position)
             }
             // 更新播放按钮的图标
             musicAdapter.notifyItemChanged(position)
         }
 
         musicRecyclerView.apply {
-            layoutManager = LinearLayoutManager(context)
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
             adapter = musicAdapter
+
         }
+
+        // 实现无限滚动
+        musicRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                val visibleItemCount = layoutManager.childCount
+                val totalItemCount = layoutManager.itemCount
+                val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+
+                if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount && firstVisibleItemPosition >= 0) {
+                    // 到达列表底部，可以加载更多数据
+                    // 调用加载更多数据的方法
+                    loadMoreData()
+                }
+            }
+        })
+
+        // 将原始音乐列表中的数据添加到音乐列表中
+        loadMoreData()
 
         return view
     }
+    private fun loadMoreData() {
+        // 将新数据添加到音乐列表中
+        musicList.addAll(originalMusicList)
 
-    private fun playMusic(resourceId: Int) {
-        if (mediaPlayer == null) {
+        // 通知适配器数据发生变化
+        musicAdapter.notifyDataSetChanged()
+    }
+
+    private fun playMusic(resourceId: Int, position: Int) {
+        if (mediaPlayer != null && isPlaying && currentPlayingPosition == position) {
+            // 如果当前有音乐在播放，且点击的是正在播放的音乐，则暂停音乐
+            mediaPlayer?.pause()
+            isPlaying = false
+        } else {
+            // 否则，停止当前正在播放的音乐（如果有），然后播放新的音乐
+            stopMusic()
             mediaPlayer = MediaPlayer.create(context, resourceId)
             mediaPlayer?.setOnCompletionListener {
                 stopMusic()
             }
+            mediaPlayer?.start()
+            isPlaying = true
+            currentPlayingPosition = position
         }
-        mediaPlayer?.start()
-        isPlaying = true
+        // 更新播放按钮的图标
+        musicAdapter.notifyItemChanged(position)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopMusic()
     }
 
     private fun stopMusic() {
@@ -101,9 +153,4 @@ class MusicFragment : Fragment() {
         isPlaying = false
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        mediaPlayer?.release()
-        mediaPlayer = null
-    }
 }
--- app/src/main/java/nju/dsy/shiduapp/music/MusicAdapter.kt
+++ app/src/main/java/nju/dsy/shiduapp/music/MusicAdapter.kt
@@ -1,4 +1,37 @@
 package nju.dsy.shiduapp.music
 
-class MusicAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import nju.dsy.shiduapp.R
+
+class MusicAdapter(
+    private val musicList: List<Music>,
+    private val onPlayClickListener: (Int) -> Unit
+) :
+    RecyclerView.Adapter<MusicAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val musicNameTextView: TextView = view.findViewById(R.id.musicNameTextView)
+        val playButton: ImageButton = view.findViewById(R.id.playButton)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.music_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val music = musicList[position]
+        holder.musicNameTextView.text = music.name
+        holder.playButton.setOnClickListener { onPlayClickListener(position) }
+    }
+
+    override fun getItemCount(): Int {
+        return musicList.size
+    }
 }
